2a358d289552c46f7d8e6455ac8585ad7cfb364a
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2023-04-08 16:28:04 +0900

    んほおおおおおおお

--- client/App.tsx
+++ client/App.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { Control } from './Control';
 import { Display } from './Display';
+import { styled } from '@mui/material/styles';
+import { Box } from '@mui/material';
 
 export const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/display" element={<Display />} />
-        <Route path="*" element={<Control />} />
-      </Routes>
-    </BrowserRouter>
+    <Wrapper>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/display" element={<Display />} />
+          <Route path="*" element={<Control />} />
+        </Routes>
+      </BrowserRouter>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled(Box)`
+  margin: 8px;
+`;
